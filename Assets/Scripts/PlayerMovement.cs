b1489e0693a22404f01a0c6e33a8ea1bf7ca15d4
Jesper Andersen <Jesper Andersen>
2016-03-07 12:29:43 +0100

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            Vector3 shootDirection = Input.mousePosition;
+            shootDirection.z = 0.0f;
+            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+            shootDirection = shootDirection - transform.position;
+            //Debug.Log(shootDirection.x + " - " + shootDirection.y + " - " + shootDirection.z);
+            //transform.position += new Vector3(shootDirection.x, shootDirection.y);
+            GameObject bullet = (GameObject) Instantiate(weapon, transform.position, Quaternion.identity);
+
+            //Debug.Log(
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Shot : MonoBehaviour {
+
+    public float speed;
+    public float lifeTime = 1;
+    private float lifeBegun;
+
+    // Use this for initialization
+    void Start () {
+        lifeBegun = Time.time;
+        Debug.Log(transform.eulerAngles.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > (lifeBegun + lifeTime)) {
+            Destroy(gameObject);
+        } else {
+            float rot = transform.eulerAngles.z;
+            transform.position += new Vector3(-(float) Math.Sin(degreeToRadian(rot)) * speed * Time.deltaTime,
+                (float) Math.Cos(degreeToRadian(rot)) * speed * Time.deltaTime);
+        }
+    }
+
+    private float degreeToRadian(float degree) {
+        return (float) ((Math.PI / 180) * degree);
+    }
+}
